fc08b79690da7a4cd1b609d8e85d1db12123ef75
Peiming WU <wupm55@126.com>
2025-10-29 13:44:31 +0800

    Initial Commit

---
+++ Image Resizer/Image_ResizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Image_ResizerApp.swift
+//  Image Resizer
+//
+//  Created by 126 on 10/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Image_ResizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Image ResizerTests/Image_ResizerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Image_ResizerTests.swift
+//  Image ResizerTests
+//
+//  Created by 126 on 10/29/25.
+//
+
+import Testing
+@testable import Image_Resizer
+
+struct Image_ResizerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
